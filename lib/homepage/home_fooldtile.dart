24e5a2712a937a6b587a3be62324c960c5b8b75f
qoranreh <qoranreh@gmail.com>
2025-01-14 17:03:21 +0900

    이미지 리스트 추가

--- lib/homepage/home_fooldtile.dart
+++ lib/homepage/home_fooldtile.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
 class HomeFooldtile extends StatelessWidget {
-  const HomeFooldtile({super.key});
+   HomeFooldtile({super.key});
+
+   //이미지 파일 불러오기 리스트
+  final List<String> imagePaths = List.generate(
+    10, // 이미지 파일 갯수
+        (index) => 'assets/FT${index + 1}.jpg',
+  );
+
 
   @override
   Widget build(BuildContext context) {
     return SliverGrid(
       delegate: SliverChildBuilderDelegate(
               (c,i) => Container(color: Colors.grey,),// c, i 넣어주기 , Container반환함 .
-          childCount: 8//=itemCount랑 비슷
+          childCount: 10//=itemCount랑 비슷
       ),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
       //가로에 몇칸
     );
   }
