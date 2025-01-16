6453d27840f82bba53800992b36630e9ce7ce2a2
qoranreh <qoranreh@gmail.com>
2025-01-16 14:46:28 +0900

    머지용 푸시

--- lib/login/Login.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';//fgggggg
-
-class appData{
-
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,2 @@
 import 'package:flutter/material.dart';
 
-//페이지 임포트
-import 'package:coupangeats/homepage/homePage.dart';
-import 'theme.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: theme,
-      home: const Homepage(),
-    );
-  }
-}
-
