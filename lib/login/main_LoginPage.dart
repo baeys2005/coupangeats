c64d1f5b044c5e763c8c35338840ce0162d349e3
baeys2005 <baeys2005@gmail.com>
2025-01-16 14:37:30 +0900

    fear: firebase 연동

---
+++ lib/login/main_LoginPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MainLoginpage extends StatefulWidget {
+  const MainLoginpage({super.key});
+
+  @override
+  State<MainLoginpage> createState() => _MainLoginpageState();
+}
+
+class _MainLoginpageState extends State<MainLoginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        
+      ),
+
+    )
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:coupangeats/login/main_LoginPage.dart';
 
-void main() {
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: MainLoginpage(),
     );
   }
 }
